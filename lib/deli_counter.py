969e0c08e2339ab3d1b77b8b722ab61d04cdf406
Landon <landoncramer1@gmail.com>
2024-08-29 11:16:07 -0700

    practice

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,3 +1,27 @@
+# katz_deli = []
+
+# def line(katz_deli):
+#     if not katz_deli:
+#         print("The line is currently empty.")
+#     else:
+#         message = "The line is currently:"
+#         for i in range(len(katz_deli)):
+#             message += f' {i + 1}. {katz_deli[i]}'
+#         print(message)
+
+# def take_a_number(katz_deli, name):
+#     katz_deli.append(name)
+#     print(f'Welcome, {name}. ' +\
+#         f'You are number {len(katz_deli)} in line.')
+    
+# def now_serving(katz_deli):
+#     if not katz_deli:
+#         print("There is nobody waiting to be served!")
+#     else:
+#         print(f"Currently serving {katz_deli[0]}.")
+#         del katz_deli[0]
+
+
 katz_deli = []
 
 def line(katz_deli):
@@ -8,15 +32,17 @@ def line(katz_deli):
         for i in range(len(katz_deli)):
             message += f' {i + 1}. {katz_deli[i]}'
         print(message)
-
+            
 def take_a_number(katz_deli, name):
     katz_deli.append(name)
     print(f'Welcome, {name}. ' +\
-        f'You are number {len(katz_deli)} in line.')
-    
+          f'You are number {len(katz_deli)} in line.')
 def now_serving(katz_deli):
     if not katz_deli:
         print("There is nobody waiting to be served!")
     else:
+        
         print(f"Currently serving {katz_deli[0]}.")
         del katz_deli[0]
+    
+# take_a_number(katz_deli, "Ada")
